439f4dfd41c5f35591490d2619ae09a494f306bc
Valentin Duplan <valentin.duplan@gmail.com>
2022-11-09 16:56:46 +0100

    back end finish

---
+++ affichagevacance.php
@@ -0,0 +1,221 @@
+<?php
+/**
+ * 2007-2022 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author    PrestaShop SA <contact@prestashop.com>
+ *  @copyright 2007-2022 PrestaShop SA
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Affichagevacance extends Module
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'affichagevacance';
+        $this->tab = 'administration';
+        $this->version = '1.0.0';
+        $this->author = 'Valentin Duplan';
+        $this->need_instance = 0;
+
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Affichage vacances');
+        $this->description = $this->l('Permet de plannifier et de definir un message de vacance sur la page daccueil');
+
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+    }
+
+    /**
+     * Don't forget to create update methods if needed:
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     */
+    public function install()
+    {
+        Configuration::updateValue('AFFICHAGEVACANCE_TEXT', '');
+        Configuration::updateValue('AFFICHAGEVACANCE_FROM', '');
+        Configuration::updateValue('AFFICHAGEVACANCE_TO', '');
+
+        return parent::install() &&
+            $this->registerHook('header') &&
+            $this->registerHook('backOfficeHeader') &&
+            $this->registerHook('displayHome');
+    }
+
+    public function uninstall()
+    {
+        Configuration::deleteByName('AFFICHAGEVACANCE_TEXT');
+        Configuration::deleteByName('AFFICHAGEVACANCE_FROM');
+        Configuration::deleteByName('AFFICHAGEVACANCE_TO');
+
+        return parent::uninstall();
+    }
+
+    /**
+     * Load the configuration form
+     */
+    public function getContent()
+    {
+        /**
+         * If values have been submitted in the form, process.
+         */
+        if (((bool) Tools::isSubmit('submitAffichagevacanceModule')) == true) {
+            $this->postProcess();
+        }
+
+        $this->context->smarty->assign('module_dir', $this->_path);
+
+        $output = $this->context->smarty->fetch($this->local_path . 'views/templates/admin/configure.tpl');
+
+        return $output . $this->renderForm();
+    }
+
+    /**
+     * Create the form that will be displayed in the configuration of your module.
+     */
+    protected function renderForm()
+    {
+        $helper = new HelperForm();
+
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submitAffichagevacanceModule';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfigFormValues(),
+            /* Add values for your inputs */
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($this->getConfigForm()));
+    }
+
+    /**
+     * Create the structure of your form.
+     */
+    protected function getConfigForm()
+    {
+        return array(
+            'form' => array(
+                'legend' => array(
+                    'title' => $this->l('Paramétrage affichage vacances'),
+                    'icon' => 'icon-cogs',
+                ),
+                'input' => array(
+                    array(
+                        'col' => 6,
+                        'type' => 'textarea',
+                        'label' => $this->l('Message vacances'),
+                        'name' => 'AFFICHAGEVACANCE_TEXT',
+                        'tinymce' => true,
+                        'cols' => 50,
+                        'rows' => 10,
+                        'autoload_rte' => true,
+                    ),
+                    array(
+                        'type' => 'datetime',
+                        //'prefix' => '<i class="icon icon-envelope"></i>',
+                        'label' => $this->l('Date debut affichage'),
+                        'name' => 'AFFICHAGEVACANCE_FROM',
+                        'required' => true,
+                    ),
+                    array(
+                        'type' => 'datetime',
+                        //'prefix' => '<i class="icon icon-envelope"></i>',
+                        'label' => $this->l('Date fin affichage'),
+                        'name' => 'AFFICHAGEVACANCE_TO',
+                        'required' => true,
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                ),
+            ),
+        );
+    }
+
+    /**
+     * Set values for the inputs.
+     */
+    protected function getConfigFormValues()
+    {
+        return array(
+            'AFFICHAGEVACANCE_TEXT' => Configuration::get('AFFICHAGEVACANCE_TEXT'),
+            'AFFICHAGEVACANCE_FROM' => Configuration::get('AFFICHAGEVACANCE_FROM'),
+            'AFFICHAGEVACANCE_TO' => Configuration::get('AFFICHAGEVACANCE_TO'),
+        );
+    }
+
+    /**
+     * Save form data.
+     */
+    protected function postProcess()
+    {
+        $form_values = $this->getConfigFormValues();
+
+        foreach (array_keys($form_values) as $key) {
+            Configuration::updateValue($key, Tools::getValue($key));
+        }
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be loaded in the BO.
+     */
+    public function hookBackOfficeHeader()
+    {
+        if (Tools::getValue('module_name') == $this->name) {
+            $this->context->controller->addJS($this->_path . 'views/js/back.js');
+            $this->context->controller->addCSS($this->_path . 'views/css/back.css');
+        }
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be added on the FO.
+     */
+    public function hookHeader()
+    {
+        $this->context->controller->addJS($this->_path . '/views/js/front.js');
+        $this->context->controller->addCSS($this->_path . '/views/css/front.css');
+    }
+
+    public function hookDisplayHome()
+    {
+        /* Place your code here. */
+    }
+}
